aa61fc9350349e9ef0b489597d174ea9c114ed43
traserelax <khar4enko.maximus@yandex.ru>
2023-12-26 17:57:13 +0300

    Лаба 2

--- lab_2_task_4.sql
+++ lab_2_task_4.sql
@@ -1,9 +1,10 @@
--- 4. Найти цвета из таблицы Production.Product, где суммарная и средняя стоимость товаров больше 1000 и 100 соответственно (из выборки исключить пустые поля).
+-- 4. Найти цвета из таблицы Production.Product, где суммарная и средняя 
+-- стоимость товаров больше 1000 и 100 соответственно (из выборки исключить пустые поля).
 SELECT "Color"
 FROM "Production"."Product"
 WHERE "StandardCost" > 100
+      AND "Color" IS NOT NULL 
+      AND "Color" != ''
 GROUP BY "Color"
 HAVING SUM("StandardCost") > 1000 
-			AND AVG("StandardCost") > 100
-			AND "Color" IS NOT NULL 
-			AND "Color" != '';
+      AND AVG("StandardCost") > 100;
