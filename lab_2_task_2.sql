eb96abb9376059165ae630a957d573e81815fe94
traserelax <khar4enko.maximus@yandex.ru>
2024-02-13 00:30:59 +0300

    Update lab_2_task_2.sql

--- lab_2_task_2.sql
+++ lab_2_task_2.sql
@@ -1,4 +1,8 @@
 -- 2. Найти среднюю и суммарную стоимость, которая больше 10, из таблицы Production.ProductCostHistory.
-SELECT AVG("StandardCost") AS "AverageCost", SUM("StandardCost") AS "TotalCost"
-FROM "Production"."ProductCostHistory"
-WHERE "StandardCost" > 10;
+SELECT
+    AVG("StandardCost") AS AverageCost, 
+    SUM("StandardCost") AS TotalCost
+FROM
+    "Production"."ProductCostHistory"
+WHERE
+    "StandardCost" > 10;
