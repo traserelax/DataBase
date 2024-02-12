e984c3bd3ead3a7ee4d8f1140dcd41dcab55cf94
traserelax <khar4enko.maximus@yandex.ru>
2024-02-13 00:25:59 +0300

    Update lab_4_task_3.sql

--- lab_4_task_3.sql
+++ lab_4_task_3.sql
@@ -3,16 +3,22 @@
   Показать товары, для которых существует только один стиль и они доступны только в одном цвете из таблицы Production.Product.
   Показать поля [Name], Style и Color.
 */
-SELECT 
-    "Name", 
-    "Style", 
-    "Color" 
-FROM 
-    "Production"."Product" 
-WHERE 
-    "Style" IS NOT NULL 
-    AND "Color" IS NOT NULL
-GROUP BY 
-    "Style", "Color", "Name"
-HAVING 
-    COUNT(*) = 1;
+SELECT
+    p1."Name",
+    p1."Style",
+    p1."Color"
+FROM
+    "Production"."Product" AS p1
+WHERE
+    p1."Style" IS NOT NULL
+    AND p1."Color" IS NOT NULL
+    AND (
+      SELECT COUNT(DISTINCT p2."Style")
+        FROM "Production"."Product" AS p2
+        WHERE p2."ProductID" = p1."ProductID"
+        ) = 1
+    AND (
+      SELECT COUNT(DISTINCT p2."Color")
+        FROM "Production"."Product" AS p2
+        WHERE p2."ProductID" = p1."ProductID"
+        ) = 1;
